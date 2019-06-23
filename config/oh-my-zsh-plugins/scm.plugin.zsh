e7e91616be424f99558dbdaf6dd7b5d0aea6c838
Sebastian Sdorra <s.sdorra@gmail.com>
2019-06-23 11:23:58 +0200

    added functions to display and go to scm repository root

---
+++ config/oh-my-zsh-plugins/scm.plugin.zsh
@@ -0,0 +1,25 @@
+scm-root () {
+  ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+  if [ "$?" != 0 ]; then
+    ROOT=$(hg root 2>/dev/null)
+    if [ "$?" != 0 ]; then
+      ROOT=$(svn info . --show-item wc-root --no-newline 2>/dev/null)
+    fi
+  fi
+  if [ -z "${ROOT}" ]; then
+    echo "no repository root found"
+    return 1
+  else
+    cd "${ROOT}"
+  fi
+}
+
+goto-scm-root () {
+  ROOT=$(scm-root)
+  if [ "$?" != 0 ]; then
+    echo "no repository root found"
+    return 1
+  else
+    cd "${ROOT}"
+  fi
+}
