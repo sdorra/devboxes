9f8f9beae6c632cfe9319a46f7343534743736f0
Sebastian Sdorra <s.sdorra@gmail.com>
2019-09-23 22:00:35 +0200

    fix switch java on osx and added java home to path

--- config/oh-my-zsh-plugins/java-env.plugin.zsh
+++ config/oh-my-zsh-plugins/java-env.plugin.zsh
@@ -1,23 +1,53 @@
-switch-java () {
+switch-java() {
   VERSION="${1}"
   if [ -n "${VERSION}" ]; then
-    DIRECTORIES=(
-      "/usr/lib/jvm/java-1.${VERSION}.0-openjdk-amd64"
-      "/usr/lib/jvm/java-${VERSION}-openjdk"
-    )
     NEW_HOME=""
-    for DIRECTORY in ${DIRECTORIES}; do
-      if [ -d "${DIRECTORY}" ]; then
-        NEW_HOME="${DIRECTORY}"
-        break
-      fi
-    done
+    if [ -f "/usr/libexec/java_home" ]; then
+      NEW_HOME=$(osx_java_home "${VERSION}")
+    else
+      NEW_HOME=$(linux_java_home "${VERSION}")
+    fi
     if [ ! -n "${NEW_HOME}" ]; then
       echo "java version ${VERSION} is not installed"
     else
+      echo "switching to java ${NEW_HOME}"
+      PATH=$(create_path "${NEW_HOME}")
       JAVA_HOME="${NEW_HOME}"
+      java -version
     fi
   else
     echo "usage switch-java version"
   fi
+}
+
+create_path() {
+  echo -n "${1}/bin"
+  segments=("${(@s/:/)PATH}")
+  for segment in "${segments[@]}"; do
+    if [ "${segment}" != "${JAVA_HOME}/bin" ]; then
+      echo -n ":${segment}"
+    fi
+  done
+}
+
+osx_java_home() {
+  VERSION="${1}"
+  if (( $VERSION > 2 && $VERSION < 9)); then
+    VERSION="1.${1}"
+  fi
+  /usr/libexec/java_home -v "${VERSION}"
+}
+
+linux_java_home() {
+  VERSION="${1}"
+  DIRECTORIES=(
+    "/usr/lib/jvm/java-1.${VERSION}.0-openjdk-amd64"
+    "/usr/lib/jvm/java-${VERSION}-openjdk"
+  )
+  for DIRECTORY in ${DIRECTORIES}; do
+    if [ -d "${DIRECTORY}" ]; then
+      echo "${DIRECTORY}"
+      break
+    fi
+  done
 }
